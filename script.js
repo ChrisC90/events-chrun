f81bbf888c5f8192fda3684702598f15a467856b
Christopher Chrun <chrischrun@gmail.com>
2024-01-01 09:34:37 -0800

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function addItem() {
+    var newItem = document.getElementById('newItem').value;
+
+    if (newItem.trim() !== '') {
+        var li = document.createElement('li');
+        li.textContent = newItem;
+
+        document.getElementById('shoppingList').appendChild(li);
+
+        document.getElementById('newItem').value = '';
+    }
+}
